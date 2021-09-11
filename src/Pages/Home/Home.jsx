c4460628756a8ad37bf854b03204915de6c16556
sajjad sadiq <sajjadsadiq2020@gmail.com>
2021-09-11 23:57:57 +0600

    done news portal website

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import {useState, useEffect} from "react";
 import "./Home.css"
 import { Header } from '../../components/Header/Header'
 import { Posts } from '../../components/Posts/Posts'
 import { SideBar } from '../../components/SideBar/SideBar'
-
+import axios from "axios"
 export const Home = () => {
+    const [posts, setPostos] = useState([])
+    useEffect(() =>{
+        const fatchPosts = async ()=>{
+            const res = await axios.get("/posts")
+            console.log("hello");
+            console.log(res);
+        }
+        fatchPosts()
+    }, [])
     return (
         <div>
             <Header/>
