39299c3dbfcd722d0e1d045089f4ad8b5756f426
Sajjad Sadiq <65564086+sajjadsadiqofficial@users.noreply.github.com>
2021-10-13 18:33:04 +0600

    update herder section update herder section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
-import "./Header.css"
+import "./Header.css";
 
 export const Header = () => {
-    return (
-        <div className="header">
-            <div className="headerTitle">
-                <h2 className="headerTitleContentLr">Enjoy</h2>
-                <h5 className="headerTitleContentSm">Taza News Dhaka City.</h5>
-            </div>
-            <div className="headerImg">
-                <img className="HeaderImgContent" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo5U6_npdLWZ6NsYIGHl7UDfM7Ixn4N2Wt3g&usqp=CAU" alt="" />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="headerTitle">
+        <h2 className="headerTitleContentLr">Enjoy Khobor Area!</h2>
+        <h5 className="headerTitleContentSm">Taza News Dhaka City.</h5>
+      </div>
+      <div className="headerImg">
+        <img
+          className="HeaderImgContent"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo5U6_npdLWZ6NsYIGHl7UDfM7Ixn4N2Wt3g&usqp=CAU"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
